3620fb6e5ba1dae41bf3766617dbc2a5262415c5
dmpdump <dmpdump@tuta.io>
2024-07-21 21:51:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ INT main(INT argc, PCHAR argv[])
 	PCHAR DllPath = argv[2];
 	BOOL ReturnValue = FALSE;
 
-        INT Retval = Inj(Pid, DllPath);
+        INT ReturnValue = Inj(Pid, DllPath);
 		
 	return 0;
 }
